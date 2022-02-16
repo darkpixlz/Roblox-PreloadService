053a44d2e9a5d64d77b54c374f39ac6a0895835a
Pixlz <91509874+darkpixlz@users.noreply.github.com>
2022-02-16 18:35:42 -0500

    About
    About file. Can be placed in any script.

---
+++ About.lua
@@ -0,0 +1,57 @@
+--[[
+
+██████╗ ██████╗ ███████╗██╗      ██████╗  █████╗ ██████╗ ███████╗███████╗██████╗ ██╗   ██╗██╗ ██████╗███████╗
+██╔══██╗██╔══██╗██╔════╝██║     ██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔════╝██╔══██╗██║   ██║██║██╔════╝██╔════╝
+██████╔╝██████╔╝█████╗  ██║     ██║   ██║███████║██║  ██║███████╗█████╗  ██████╔╝██║   ██║██║██║     █████╗  
+██╔═══╝ ██╔══██╗██╔══╝  ██║     ██║   ██║██╔══██║██║  ██║╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██║██║     ██╔══╝  
+██║     ██║  ██║███████╗███████╗╚██████╔╝██║  ██║██████╔╝███████║███████╗██║  ██║ ╚████╔╝ ██║╚██████╗███████╗
+╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝ ╚═════╝╚══════╝
+                                                                                                             
+                                                                                                             v1.1 (2/16/22 build, BETA GITHUB VERSION)
+                                                                                                             
+                                                                                                             
+Hey, thanks for using PreloadService!
+
+Everything is made by DarkPixlz. That includes all UI, Images used, and code.
+https://devforum.roblox.com/t/preloadservice-load-assets-fast-via-contentprovider-v1/1663119
+https://devforum.roblox.com/u/DarkPixlz
+Roblox: https://www.roblox.com/users/133017837/profile
+Twitter: @darkpixlz
+Youtube: https://www.youtube.com/channel/UCDD8yUcnPN17yOR17fifpxA
+Discord: @Pixlz#4627
+
+SETTINGS:
+game/ReplicatedStorage/PreloadService/Settings (at least by default, you may change it)
+Modify settings by chainging their values in the table. Renaming them will break the module!
+
+
+SPECIAL THANKS:
+Nobody yet :(
+
+
+INFO:
+
+You may use all of my code, and UI.
+DO NOT RESELL!!!
+DO NOT CLAIM AS YOUR OWN!!
+DO NOT REUPLOAD WITH VIRUSES!!!!
+If you can, avoid using my UI for loading outside of PreloadingService.
+
+DONATE: 
+You can donate here. This helps the development of this module for more features!
+https://www.roblox.com/game-pass/28730610/Donate-to-PreloadService
+
+UPDATING: To keep the module up to date, just add this code to any script (best in a seperate one)
+require(game.ReplicatedStorage.PreloadService).CheckVersion()
+That's all! It will print the result.
+
+FEEDBACK/BUGS:
+
+Go to the DevForum topic, and make a reply. You can also reach out to me on Discord or Twitter.
+
+Once again, thanks for using this module! Hope you cna enjoy it.
+]]
+
+
+
+script:Destroy()

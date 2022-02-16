aeadf03292c68279ac31d6083a0db262a2194f2e
Pixlz <91509874+darkpixlz@users.noreply.github.com>
2022-02-16 18:33:44 -0500

    Settings.lua
    PLACE AS THE FIRST CHILD TO THE MAIN MODULE!
    Name it "Settings".

---
+++ Settings.lua
@@ -0,0 +1,10 @@
+local settings = {
+	PrintData = true,
+	ShutdownIfOutofDate = false,
+	UseTweens = true,
+	LightDefaultUI = true,
+	StartDelay = 15,
+	InBetweenAssetsDelay = 5,
+	AutoCloseUI = true,
+}
+return settings
